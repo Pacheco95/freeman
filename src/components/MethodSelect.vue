bd2a6deae51bc052fa1f309487d6c738884425d3
Michael Pacheco <mdpgd95@gmail.com>
2026-03-23 11:23:45 -0300

    feat: add MethodSelect.vue component

---
+++ src/components/MethodSelect.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+const methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'] as const
+export type Method = (typeof methods)[number]
+defineProps<{ modelValue: Method }>()
+const emit = defineEmits<{ 'update:modelValue': [value: Method] }>()
+</script>
+
+<template>
+  <Select
+    :model-value="modelValue"
+    @update:model-value="(val) => emit('update:modelValue', val as Method)"
+  >
+    <SelectTrigger class="w-30">
+      <SelectValue placeholder="Select a method" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectItem v-for="method in methods" :value="method" :key="method">{{ method }} </SelectItem>
+    </SelectContent>
+  </Select>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MethodSelect, { type Method } from '@/components/MethodSelect.vue'
+import { ref } from 'vue'
+
+const method = ref<Method>('GET')
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <MethodSelect :model-value="method" @update:model-value="method = $event" />
+  </main>
 </template>
